81d474be0b31011886767a5579106bdafd76825f
dweil29 <dweil@students.mchenry.edu>
2017-02-02 10:42:59 -0600

    Initial commit

---
+++ initializevariables/src/main/java/com/example/MyVariables.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class MyVariables
+{
+    public static void main(String[] args)
+    {
+        /* This program creates a variable of each type and prints it out.
+            - Dakoda Weil
+         */
+        byte b = 8;
+        System.out.println(b);
+        short s = 17;
+        System.out.println(s);
+        int i = 357;
+        System.out.println(i);
+        long l = 33548994742992L;
+        System.out.println(l);
+        float f = 12.2f;
+        System.out.println(f);
+        double d = 3.14159265;
+        System.out.println(d);
+        boolean b1 = true;
+        System.out.println(b1);
+        char c = 'a';
+        System.out.println(c);
+    }
+
+
+}
